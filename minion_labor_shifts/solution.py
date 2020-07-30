35374ea8bb9a440631a9a85b8ebd804617fc2afc
Matt Hosch <matt_hosch@hotmail.com>
2020-07-29 20:05:08 -0500

    Added test_cases, improved numbers_station solution.

--- minion_labor_shifts/solution.py
+++ minion_labor_shifts/solution.py
@@ -1,4 +1,11 @@
 def answer(data, n):
 	return list(filter(lambda x: data.count(x) <= n, data))
 	
-print(answer([5, 10, 15, 10, 7], 1))
+def test_case_1():
+    assert answer([1, 2, 3], 0) == []
+
+def test_case_2():
+    assert answer([1, 2, 2, 3, 3, 3, 4, 5, 5], 1) == [1, 4]
+
+def test_case_3():
+    assert answer([1, 2, 3], 6) == [1, 2, 3]
--- numbers_stations_coded_messages/solution.py
+++ numbers_stations_coded_messages/solution.py
@@ -1,13 +1,29 @@
 def answer(l, t):
-	output = [-1, -1]
-	for x in range(len(l)):
-		for y in range(len(l)):
-			if sum(l[x:y+1]) == t:
-				return [x, y]
-			#print(x, y, sum(l[x:y+1]))
-	return output
-	
-print answer([4, 3, 5, 7, 8], 12) #expect [0, 2]
-print answer([4, 3, 10, 2, 8], 12) #expect [2, 3]
-print answer([1, 2, 3, 4], 15) #expect [-1, -1]
+    # Store key values in dictionary with sum:index
+    dict = {}
+    # Initialize with 0: -1 in case sublist starts from index 0
+    dict[0] = -1
+    sum = 0
+    # traverse the given list
+    for i in range(len(l)):
+        # sum of elements so far
+        sum += l[i]
+        # If the sum hasn't been recorded, add to dict
+        if sum not in dict:
+            dict[sum] = i
+        # If sum - t exists in dict, we've found a valid solution
+        if sum - t in dict:
+            # Subtract index of sum - t
+            length = i - dict[sum - t]
+            return [i - length + 1, i]
+    # No valid values found
+    return [-1, -1]
 
+def test_case_1():
+    assert answer([4, 3, 5, 7, 8], 12) == [0, 2]
+
+def test_case_2():
+    assert answer([4, 3, 10, 2, 8], 12) == [2, 3]
+
+def test_case_3():
+    assert answer([1, 2, 3, 4], 15) == [-1, -1] 
